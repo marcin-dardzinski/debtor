7e98c54a44e263b1b2e9fa34669e96a8238b06af
Marcin <mdardzinski96@gmail.com>
2019-05-14 23:43:39 +0200

    Display friends balances

--- mobile/lib/pages/friends_page.dart
+++ mobile/lib/pages/friends_page.dart
@@ -2,6 +2,7 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:debtor/authenticator.dart';
 import 'package:debtor/friends_service.dart';
 import 'package:debtor/helpers.dart';
+import 'package:debtor/models/balance.dart';
 import 'package:debtor/models/user.dart';
 import 'package:decimal/decimal.dart';
 import 'package:flutter/material.dart';
@@ -12,11 +13,24 @@ import 'package:tuple/tuple.dart';
 Authenticator authenticator = Authenticator();
 FriendsService friends = FriendsService();
 
-Observable<Tuple2<AuthenticationState, Decimal>> _combineUserAndExpenses() {
+Observable<Tuple2<AuthenticationState, Decimal>> _combineUserAndTotalBalance() {
   return Observable.combineLatest2(
       authenticator.loggedInUser, friends.myTotalBalance,
       (AuthenticationState auth, Decimal balance) {
     return Tuple2(auth, balance);
+  });
+}
+
+Observable<List<Tuple2<User, Decimal>>> _combineFriendsAndBalaces() {
+  return Observable.combineLatest2(friends.friends, friends.myBalances,
+      (List<User> friends, List<Balance> balances) {
+    final balancesMap = Map<String, Decimal>.fromIterable(balances,
+        key: (dynamic b) => b.otherUserId, value: (dynamic b) => b.amount);
+
+    return friends.map((friend) {
+      final amount = balancesMap[friend.uid] ?? Decimal.fromInt(0);
+      return Tuple2(friend, amount);
+    }).toList();
   });
 }
 
@@ -26,7 +40,7 @@ class FriendsPage extends StatelessWidget {
     return Column(
       children: [
         StreamBuilder<Tuple2<AuthenticationState, Decimal>>(
-            stream: _combineUserAndExpenses(),
+            stream: _combineUserAndTotalBalance(),
             builder: (ctx, snapshot) {
               if (snapshot.hasData && snapshot.data.item1.user != null) {
                 return CurrentUserBar(
@@ -37,8 +51,8 @@ class FriendsPage extends StatelessWidget {
               return Container();
             }),
         const Divider(),
-        StreamBuilder<List<User>>(
-          stream: friends.friends,
+        StreamBuilder<List<Tuple2<User, Decimal>>>(
+          stream: _combineFriendsAndBalaces(),
           builder: (ctx, snapshot) {
             if (!snapshot.hasData) {
               return Container();
@@ -49,7 +63,8 @@ class FriendsPage extends StatelessWidget {
                   itemCount: snapshot.data.length + 1,
                   itemBuilder: (ctx, idx) {
                     return idx < snapshot.data.length
-                        ? _friendTile(snapshot.data[idx])
+                        ? _friendTile(
+                            snapshot.data[idx].item1, snapshot.data[idx].item2)
                         : _addFriendButton(ctx);
                   }),
             );
@@ -59,13 +74,19 @@ class FriendsPage extends StatelessWidget {
     );
   }
 
-  Widget _friendTile(User user) {
+  Widget _friendTile(User user, Decimal balance) {
     return ListTile(
       leading: CircleAvatar(
         backgroundImage: CachedNetworkImageProvider(user.avatar),
       ),
       title: Text(user.name),
       subtitle: Text(user.email),
+      trailing: Container(
+          margin: const EdgeInsets.only(right: 8),
+          child: Text(
+            balance.toStringAsFixed(2),
+            style: TextStyle(color: getColorForBalance(balance)),
+          )),
     );
   }
 
